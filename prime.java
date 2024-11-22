7c63e30d3b1f1437ee29926c76b6881ff3dff6fd
Venky204 <venkatasaivenky9@gmail.com>
2024-11-22 14:38:03 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -7,24 +7,10 @@ public class prime {
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
 
-        if (isPrime(number)) {
-            System.out.println(number + " is a prime number.");
-        } else {
-            System.out.println(number + " is not a prime number.");
-        }
+       for(int i=0;i<n;i++){
+           System.out.println("hello");
+       }
 
         scanner.close();
-    }
-
-    public static boolean isPrime(int num) {
-        if (num <= 1) {
-            return false; // 0 and 1 are not prime numbers
-        }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false; // Found a factor, not prime
-            }
-        }
-        return true; // No factors found, is prime
     }
 }
