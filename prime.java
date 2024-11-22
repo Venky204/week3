131b4165f768fffad433729aed7c4b2fcb7b9175
Venky204 <venkatasaivenky9@gmail.com>
2024-11-22 14:40:00 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -2,10 +2,25 @@ import java.util.Scanner;
 
 public class prime {
     public static void main(String[] args) {
-        
-           System.out.println("hello");
-       
+        int number = 21;
 
-      
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; // 0 and 1 are not prime numbers
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; // Found a factor, not prime
+            }
+        }
+        return true; // No factors found, is prime
     }
 }
+
